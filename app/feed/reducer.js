80a0de190084bdc19ce7cf72048a808da35bebf1
Delek Turner <dturner@victoria.ca>
2015-12-10 16:16:14 -0800

    preliminary reducer for feed

--- app/feed/reducer.js
+++ app/feed/reducer.js
@@ -1,3 +1,15 @@
 export default function feedReducer(state, action){
-	
+	switch (action.type){
+		case 'fetch feed':
+			return Object.assign({},state,{
+				isFetching: true
+			});
+		case 'feed received':
+			return Object.assign({},state, {
+				isFetching:false,
+				feed:action.feed
+			});
+		default:
+			return state;
+	}
 }
