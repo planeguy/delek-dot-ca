49a8bad32e7bf16ac60948f6a3d5e5704e44cbff
Delek Turner <dturner@victoria.ca>
2015-12-18 16:57:35 -0800

    started clusterfriend-loader

---
+++ app/clusterfriend-loader.js
@@ -0,0 +1,15 @@
+import poop from 'vendor/ajaxpoop';
+
+function jsFromXhr(xhr, options){
+	if(xhr.status<400) return new Promise((rs,rj)=>{rs(JSON.parse(xhr.response));}); 
+}
+
+class ClusterfriendLoader{
+	load(profileUrl){
+		//return a promise
+		Promise.resolve(profileUrl)
+		.then((url)=>poop(url).get())
+		.then((profileXhr)=>poop((JSON.parse(profileXhr.response)).feed).get())
+		.then((feedXhr)=>)
+	}
+}
