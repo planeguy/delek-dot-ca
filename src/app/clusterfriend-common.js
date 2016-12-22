38f6d48ad72fb94767a133b799633038b504c4ca
Del Turner <dturner@victoria.ca>
2016-12-22 12:25:57 -0800

    uuid maker

--- src/app/clusterfriend-common.js
+++ src/app/clusterfriend-common.js
@@ -25,3 +25,7 @@ export function getFeedFromHash(hash){
     if (parts.length>1) parts.pop();
     return parts.reduce((f,p)=>f+'/'+p);
 }
+
+export function newid() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,a=>(a^Math.random()*16>>a/4).toString(16));
+}
