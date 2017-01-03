78543ffe28da036f9e0fdebf2d8be486e58552da
Del Turner <dturner@victoria.ca>
2017-01-03 10:29:10 -0800

    set viewport (on main too)

--- src/assets/common/post/index.php
+++ src/assets/common/post/index.php
@@ -37,6 +37,7 @@ try {
     <head>
         <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
         <meta content="utf-8" http-equiv="encoding">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="../main.css"/>
     </head>
     <body>
