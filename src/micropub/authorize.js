6302a37e3fb15c20d79a8b7019590d6e7d841ab4
Del Turner <planeguy@delek.ca>
2019-08-01 10:05:54 -0700

    remove service from this repo

--- src/micropub/authorize.js
+++
@@ -1,9 +0,0 @@
-exports.authorize = function (role) {
-    return async function (ctx, next) {
-        if (ctx.req.user.roles.indexOf(role) < 0) {
-            ctx.status = 401;
-        } else {
-            await next();
-        }
-    }
-}
